fbd51c3e1274c592a9106ea614ae85402b70a33e
hamooda1995 <hamooda.syr1@gmail.com>
2026-09-24 12:55:06 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,23 @@
-// محرك تشغيل الخلفية لـ PWA حمودة
-const CACHE_NAME = 'hamooda-cache-v1';
-const ASSETS = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'simple-cache-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json'
+];
 
+// Install Service Worker and cache core files
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
+// Fetch assets from cache or network
 self.addEventListener('fetch', (e) => {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
